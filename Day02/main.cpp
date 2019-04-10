678141f88e70ca8d8b7ec96c4dd4808747f449dd
Saphronov <hijarian@gmail.com>
2019-04-10 03:52:54 +0300

    Решил день 2

--- Day02/main.cpp
+++ Day02/main.cpp
@@ -4,6 +4,24 @@
 #include <regex>
 #include <algorithm>
 
+unsigned calc_area(unsigned first_dimension, unsigned second_dimension, unsigned third_dimension)
+{
+	unsigned first_area = first_dimension * second_dimension;
+	unsigned second_area = second_dimension * third_dimension;
+	unsigned third_area = third_dimension * first_dimension;
+
+	return 2 * first_area + 2 * second_area + 2 * third_area + std::min({ first_area, second_area, third_area });
+}
+
+unsigned calc_length(unsigned first_dimension, unsigned second_dimension, unsigned third_dimension)
+{
+	unsigned ribbon = 2 * (first_dimension + second_dimension + third_dimension - std::max({ first_dimension, second_dimension, third_dimension }));
+	unsigned bow = first_dimension * second_dimension * third_dimension;
+
+	return ribbon + bow;
+}
+
+
 int main(int argc, char** argv)
 {
 	if (argc < 2)
@@ -20,7 +38,36 @@ int main(int argc, char** argv)
 		exit(2);
 	}
 
-	//TODO
+	unsigned total_area{};
+	unsigned total_length{};
+
+	unsigned first_dimension;
+	unsigned second_dimension;
+	unsigned third_dimension;
+
+	std::string line;
+	while (std::getline(input_file, line))
+	{
+		std::regex dimensions_regex{ "([0-9]+)x([0-9]+)x([0-9]+)" };
+		std::smatch dimensions_regex_match;
+
+
+		if (!std::regex_match(line, dimensions_regex_match, dimensions_regex))
+		{
+			std::cerr << "No match for line: " << line << std::endl;
+			continue;
+		}
+
+		first_dimension = std::stoul(dimensions_regex_match[1]);
+		second_dimension = std::stoul(dimensions_regex_match[2]);
+		third_dimension = std::stoul(dimensions_regex_match[3]);
+
+		total_area += calc_area(first_dimension, second_dimension, third_dimension);
+		total_length += calc_length(first_dimension, second_dimension, third_dimension);
+	}
+
+	std::cout << "Total cloth: " << total_area << std::endl;
+	std::cout << "Total ribbon: " << total_length << std::endl;
 
 	std::cin.get();
 
