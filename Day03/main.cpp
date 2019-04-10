b6be8b10b62de6dfeb861110f45888e9b3ad47ba
Saphronov <hijarian@gmail.com>
2019-04-10 03:35:26 +0300

    Решил задачу 3

--- Day03/main.cpp
+++ Day03/main.cpp
@@ -21,7 +21,84 @@ int main(int argc, char** argv)
 		exit(2);
 	}
 
-	//TODO
+	std::pair<int, int> current_position_single_santa{ 0, 0 };
+	std::set<std::pair<int, int>> visited_positions_single_santa{ std::make_pair(0, 0) };
+
+	bool is_santa_turn{ true };
+	std::pair<int, int> current_position_santa{ 0, 0 };
+	std::pair<int, int> current_position_robo{ 0, 0 };
+	std::set<std::pair<int, int>> visited_positions_santa_robo{ std::make_pair(0, 0) };
+
+	char input;
+	while (input_file.get(input))
+	{
+		if (input == '^')
+		{
+			++current_position_single_santa.second;
+			if (is_santa_turn)
+			{
+				++current_position_santa.second;
+			}
+			else
+			{
+				++current_position_robo.second;
+			}
+		}
+		if (input == '>')
+		{
+			++current_position_single_santa.first;
+			if (is_santa_turn)
+			{
+				++current_position_santa.first;
+			}
+			else
+			{
+				++current_position_robo.first;
+			}
+		}
+		if (input == '<')
+		{
+			--current_position_single_santa.first;
+			if (is_santa_turn)
+			{
+				--current_position_santa.first;
+			}
+			else
+			{
+				--current_position_robo.first;
+			}
+		}
+		if (input == 'v')
+		{
+			--current_position_single_santa.second;
+			if (is_santa_turn)
+			{
+				--current_position_santa.second;
+			}
+			else
+			{
+				--current_position_robo.second;
+			}
+		}
+		//std::cout << input << ": " << current_position_single_santa.first << " " << current_position_single_santa.second << std::endl;
+		visited_positions_single_santa.insert(current_position_single_santa);
+
+		if (is_santa_turn)
+		{
+			visited_positions_santa_robo.insert(current_position_santa);
+			//std::cout << input << ": S " << current_position_santa.first << " " << current_position_santa.second << std::endl;
+		}
+		else
+		{
+			visited_positions_santa_robo.insert(current_position_robo);
+			//std::cout << input << ": R " << current_position_robo.first << " " << current_position_robo.second << std::endl;
+		}
+		is_santa_turn = !is_santa_turn;
+	}
+
+	std::cout << "Single Santa visited houses: " << visited_positions_single_santa.size() << std::endl;
+
+	std::cout << "Santa with Robo-Santa visited houses: " << visited_positions_santa_robo.size() << std::endl;
 
 	std::cin.get();
 
